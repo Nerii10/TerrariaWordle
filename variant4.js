5fe2f2c5471d24af7b162260e68e44b0e4d6b813
Nerii10 <96701056+Nerii10@users.noreply.github.com>
2024-12-26 21:40:40 +0100

    E

--- variant4.js
+++ variant4.js
@@ -6,7 +6,6 @@ export const fadeIn4 = (direction, delay) => {
             y: direction === 'up' ? 50 : direction === 'down' ? -20 : 0,
             x: direction === 'right' ? 50 : direction === 'left' ? -20 : 0,
             opacity: 0,
-            rotate: "180deg",
             scale: 0.1,
         },
 
@@ -14,7 +13,6 @@ export const fadeIn4 = (direction, delay) => {
             y: 0,
             x: 0,
             opacity: 1,
-            rotate: "0deg",
             scale: 1,
             transition: {
                 duration: 0.5,
